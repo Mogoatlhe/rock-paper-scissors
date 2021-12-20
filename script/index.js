888e4aa545905a0e80b58304c523227145502654
mogoatlhe <iam@ofent.se>
2021-12-20 17:54:06 +0200

    implemented one round rock paper scissors game

--- script/index.js
+++ script/index.js
@@ -1,1 +1,45 @@
-console.log("Hello");
+
+let decisionArr = ["rock", "paper", "scissors"];
+
+let computePlay = () => {
+
+    let decisionPosition = parseInt(Math.random() * 3);
+
+    return decisionArr[decisionPosition];
+}
+
+let playerPlay = () => {
+
+    return prompt("Rock, Paper, or Scissors");
+}
+
+
+let playerSelection = playerPlay();
+let computerSelection = computePlay();
+
+let playRound = (playerSelection, computerSelection) => {
+
+    let result = null;
+    playerSelection = playerSelection.toLowerCase();
+    console.log(`You: ${playerSelection} Computer: ${computerSelection}`);
+    
+    // readable ? lol
+    result = (playerSelection === computerSelection) ? "tie" :
+        (playerSelection === decisionArr[0] && computerSelection === decisionArr[1]) ?
+        "You Lose! Paper beats Rock" :  
+        (playerSelection === decisionArr[0] && computerSelection === decisionArr[2]) ?
+        "You Win! Rock beats Scissors" :
+        (playerSelection === decisionArr[1] && computerSelection === decisionArr[2]) ?
+        "You Lose! Scissors beats Rock" :
+        (playerSelection === decisionArr[1] && computerSelection === decisionArr[0]) ?
+        "You win! Paper beats Rock" :
+        (playerSelection === decisionArr[2] && computerSelection === decisionArr[0]) ?
+        "You Lose! Rock beats Scissors" :
+        (playerSelection === decisionArr[2] && computerSelection === decisionArr[1]) ?
+        "You Win! Scissors beats Paper" : "Invalid input";
+
+    return result;
+
+}
+
+console.log(playRound(playerSelection, computerSelection));
