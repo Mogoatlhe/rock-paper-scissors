09663d2b696fa876d0bc557aa53d7c1daa0a7334
mogoatlhe <iam@ofent.se>
2021-12-29 11:47:45 +0200

    restructuring the html

--- script/index.js
+++ script/index.js
@@ -11,16 +11,41 @@ let computePlay = () => {
 let score = 0;
 let playerScore = 0;
 let computerScore = 0;
+let playerBtn = null;
+let computerBtn = null;
+
+function resetBtnBackgroundImage(){
+
+    let selectionBtn = null;
+
+    decisionArr.forEach(selection => {
+        
+        selectionBtn = document.querySelector(`#${selection}`);
+        selectionBtn.style.backgroundImage = `url("./images/${selection}.png")`;
+    });
+}
 
 function playRound(playerSelection, computerSelection){
     
+    resetBtnBackgroundImage();
+
     let result = null;
     let overallWinner = null;
     playerSelection = playerSelection.toLowerCase();
     
+    playerBtn = document.querySelector(`#${playerSelection}`);
+    computerBtn = document.querySelector(`#${computerSelection}`);
+
+    if(playerSelection !== computerSelection){
+        playerBtn.style.backgroundImage = `url("./images/${playerSelection}-you.png")`;
+        computerBtn.style.backgroundImage = `url("./images/${computerSelection}-computer.png")`;
+    }
+
     if(playerSelection === computerSelection){
         result = `Tie: You - ${playerSelection} : Computer - ${computerSelection} <br>
         You: ${playerScore} - ${ computerScore } :Computer`;
+        
+        playerBtn.style.backgroundImage = `url("./images/${playerSelection}.png")`;
     }else if(playerSelection === decisionArr[0] && computerSelection == decisionArr[1]){
         result = `You Lose: You - ${playerSelection} : Computer - ${computerSelection} <br>
         You: ${playerScore} - ${ ++computerScore } :Computer`;
